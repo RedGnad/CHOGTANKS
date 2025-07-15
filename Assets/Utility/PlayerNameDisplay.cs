dfee9adfc860d82d83752a319347a3ed7ba80b4e
Red.G <red.g@MacBook-Pro-de-RedG.local>
2025-07-15 07:01:51 +0200

    publicroom-update

--- Assets/Utility/PlayerNameDisplay.cs
+++ Assets/Utility/PlayerNameDisplay.cs
@@ -58,7 +58,7 @@ public class PlayerNameDisplay : MonoBehaviourPunCallbacks
                     int nftLevel = nftManager.currentNFTState.level;
                     if (nftLevel > 0)
                     {
-                        playerName += $" lvl {nftLevel}";
+                        playerName += $" lvl  {nftLevel}";
                     }
                 }
             }
